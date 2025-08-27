ccdbd9498f1b5fc6a1c4d2e87fe8cbfda033f342
Tim Lefler <tim@tlef.ca>
2025-08-27 11:24:39 -0700

    WIP Layout and UI

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,12 +1,15 @@
 import { type Context } from 'koa';
 import type Router from 'koa-router';
 import { type IRenderer } from '../libs/renderer/index.js';
+import { PostsController } from '../controllers/posts-controller.js';
 
 export class HomePage {
 	protected renderer: IRenderer;
+	protected postsController: PostsController;
 
-	constructor(renderer: IRenderer) {
+	constructor(renderer: IRenderer, postsController: PostsController) {
 		this.renderer = renderer;
+		this.postsController = postsController;
 	}
 
 	public registerRoutes(router: Router): void {
@@ -14,6 +17,7 @@ export class HomePage {
 	}
 
 	private async getHome(ctx: Context): Promise<void> {
-		ctx.body = this.renderer.render('home', { name: 'user' });
+		const posts = this.postsController.getPosts();
+		ctx.body = this.renderer.render('home', { name: 'user', posts });
 	}
 }
